d598f969daf9dd41ed72043da3c07f610faf205e
Giorgi Berozashvili <>
2022-04-16 17:16:26 +0400

    Add UIBuilder framework

---
+++ UIBuilder/UIBuilder/UIBuilder/UIBuilder.h
@@ -0,0 +1,18 @@
+//
+//  UIBuilder.h
+//  UIBuilder
+//
+//  Created by Giorgi Berozashvili on 16.04.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIBuilder.
+FOUNDATION_EXPORT double UIBuilderVersionNumber;
+
+//! Project version string for UIBuilder.
+FOUNDATION_EXPORT const unsigned char UIBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIBuilder/PublicHeader.h>
+
+
