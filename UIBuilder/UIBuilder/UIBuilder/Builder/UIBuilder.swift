5d6d5d2a5397092cd43dc81fb6c9085425381ede
giorgi-berrozashvili <59742685+giorgi-berrozashvili@users.noreply.github.com>
2022-04-20 16:42:38 +0400

    Update UIBuilder.swift
    Add layout part, unfinished

--- UIBuilder/UIBuilder/UIBuilder/Builder/UIBuilder.swift
+++ UIBuilder/UIBuilder/UIBuilder/Builder/UIBuilder.swift
@@ -17,4 +17,75 @@ open class UIBuilder<ViewType> where ViewType: UIView {
     public init(_ type: ViewType.Type) {
         self.view = type.init()
     }
+    
+    open func subviews(_ subviews: [UIView]) -> UIBuilder {
+        subviews.forEach { subview in view.addSubview(subview) }
+        return self
+    }
+    
+    @discardableResult
+    open func subview(_ subview: UIView) -> UIBuilder {
+        view.addSubview(subview)
+        return self
+    }
+    
+    @discardableResult
+    open func subview(_ subview: UIView, inset: UIEdgeInsets) -> UIBuilder {
+        view.addSubview(subview)
+        
+        return self
+    }
+    
+    @discardableResult
+    open func subview(_ subview: UIView, _ completion: () -> UILayout) -> UIBuilder {
+        view.addSubview(subview)
+        completion()
+        return self
+    }
+
+    
+    @discardableResult
+    open func fillSuperView(_ spacing: CGFloat = .zero) -> UIBuilder {
+        view.topAnchor.constraint(equalTo: view.superview!.topAnchor, constant: spacing).isActive = true
+        view.leadingAnchor.constraint(equalTo: view.superview!.leadingAnchor, constant: spacing).isActive = true
+        view.trailingAnchor.constraint(equalTo: view.superview!.trailingAnchor, constant: -spacing).isActive = true
+        view.bottomAnchor.constraint(equalTo: view.superview!.bottomAnchor, constant: -spacing).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    open func topToSuperView(_ spacing: CGFloat = .zero) -> UIBuilder {
+        view.topAnchor.constraint(equalTo: view.superview!.topAnchor, constant: spacing).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    open func leadingToSuperView(_ spacing: CGFloat = .zero) -> UIBuilder {
+        view.leadingAnchor.constraint(equalTo: view.superview!.leadingAnchor, constant: spacing).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    open func trailingToSuperView(_ spacing: CGFloat = .zero) -> UIBuilder {
+        view.trailingAnchor.constraint(equalTo: view.superview!.trailingAnchor, constant: -spacing).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    open func bottomToSuperView(_ spacing: CGFloat = .zero) -> UIBuilder {
+        view.bottomAnchor.constraint(equalTo: view.superview!.bottomAnchor, constant: -spacing).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    open func height(_ height: CGFloat) -> UIBuilder {
+        view.heightAnchor.constraint(equalToConstant: height).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    open func width(_ width: CGFloat) -> UIBuilder {
+        view.widthAnchor.constraint(equalToConstant: width).isActive = true
+        return self
+    }
 }
