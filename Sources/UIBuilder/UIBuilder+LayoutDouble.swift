ad586c4ea9335d8ffd54e9b22564637c950b9494
Giorgi Berozashvili <>
2022-04-22 15:48:39 +0400

    Add two view addition center alignment support

--- Sources/UIBuilder/UIBuilder+LayoutDouble.swift
+++ Sources/UIBuilder/UIBuilder+LayoutDouble.swift
@@ -37,10 +37,46 @@ open class UILayoutDouble<SuperView, SubView1, SubView2> where SuperView: UIView
             left: left,
             spacing: spacing,
             right: right,
-            leftTop: .zero,
-            leftBottom: .zero,
-            rightTop: .zero,
-            rightBottom: .zero
+            leftTop: nil,
+            leftBottom: nil,
+            rightTop: nil,
+            rightBottom: nil
+        )
+    }
+    
+    @discardableResult
+    open func alignHorizontally(left: CGFloat,
+                                spacing: CGFloat,
+                                right: CGFloat,
+                                leftTop: CGFloat,
+                                leftBottom: CGFloat) -> UILayoutDouble {
+        
+        return alignHorizontally(
+            left: left,
+            spacing: spacing,
+            right: right,
+            leftTop: leftTop,
+            leftBottom: leftBottom,
+            rightTop: nil,
+            rightBottom: nil
+        )
+    }
+    
+    @discardableResult
+    open func alignHorizontally(left: CGFloat,
+                                spacing: CGFloat,
+                                right: CGFloat,
+                                rightTop: CGFloat,
+                                rightBottom: CGFloat) -> UILayoutDouble {
+        
+        return alignHorizontally(
+            left: left,
+            spacing: spacing,
+            right: right,
+            leftTop: nil,
+            leftBottom: nil,
+            rightTop: rightTop,
+            rightBottom: rightBottom
         )
     }
     
@@ -66,20 +102,23 @@ open class UILayoutDouble<SuperView, SubView1, SubView2> where SuperView: UIView
     open func alignHorizontally(left: CGFloat,
                                 spacing: CGFloat,
                                 right: CGFloat,
-                                leftTop: CGFloat,
-                                leftBottom: CGFloat,
-                                rightTop: CGFloat,
-                                rightBottom: CGFloat) -> UILayoutDouble {
-        subView1.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: left).isActive = true
-        subView1.topAnchor.constraint(equalTo: superView.topAnchor, constant: leftTop).isActive = true
-        subView1.bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -leftBottom).isActive = true
+                                leftTop: CGFloat?,
+                                leftBottom: CGFloat?,
+                                rightTop: CGFloat?,
+                                rightBottom: CGFloat?) -> UILayoutDouble {
         
+        subView1.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: left).isActive = true
         subView2.trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -right).isActive = true
-        subView2.topAnchor.constraint(equalTo: superView.topAnchor, constant: rightTop).isActive = true
-        subView2.bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -rightBottom).isActive = true
+        
+        leftTop.map { subView1.topAnchor.constraint(equalTo: superView.topAnchor, constant: $0).isActive = true }
+        leftBottom.map { subView1.bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -$0).isActive = true }
+        rightTop.map { subView2.topAnchor.constraint(equalTo: superView.topAnchor, constant: $0).isActive = true }
+        rightBottom.map { subView2.bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -$0).isActive = true }
         
         subView2.leadingAnchor.constraint(equalTo: subView1.trailingAnchor, constant: spacing).isActive = true
         
+        subView1.centerYAnchor.constraint(equalTo: superView.centerYAnchor).isActive = true
+        subView2.centerYAnchor.constraint(equalTo: superView.centerYAnchor).isActive = true
         return self
     }
     
@@ -102,17 +141,55 @@ open class UILayoutDouble<SuperView, SubView1, SubView2> where SuperView: UIView
             top: top,
             spacing: spacing,
             bottom: bottom,
-            left: .zero,
-            right: .zero
+            topLeft: nil,
+            bottomLeft: nil,
+            topRight: nil,
+            bottomRight: nil
         )
     }
     
     @discardableResult
     open func alignVertically(top: CGFloat,
-                                spacing: CGFloat,
-                                bottom: CGFloat,
-                                left: CGFloat,
-                                right: CGFloat) -> UILayoutDouble {
+                              spacing: CGFloat,
+                              bottom: CGFloat,
+                              topLeft: CGFloat,
+                              topRight: CGFloat) -> UILayoutDouble {
+        
+        return alignVertically(
+            top: top,
+            spacing: spacing,
+            bottom: bottom,
+            topLeft: topLeft,
+            bottomLeft: nil,
+            topRight: topRight,
+            bottomRight: nil
+        )
+    }
+    
+    @discardableResult
+    open func alignVertically(top: CGFloat,
+                              spacing: CGFloat,
+                              bottom: CGFloat,
+                              bottomLeft: CGFloat,
+                              bottomRight: CGFloat) -> UILayoutDouble {
+        
+        return alignVertically(
+            top: top,
+            spacing: spacing,
+            bottom: bottom,
+            topLeft: nil,
+            bottomLeft: bottomLeft,
+            topRight: nil,
+            bottomRight: bottomRight
+        )
+    }
+    
+    @discardableResult
+    open func alignVertically(top: CGFloat,
+                              spacing: CGFloat,
+                              bottom: CGFloat,
+                              left: CGFloat,
+                              right: CGFloat) -> UILayoutDouble {
         
         return alignVertically(
             top: top,
@@ -129,20 +206,23 @@ open class UILayoutDouble<SuperView, SubView1, SubView2> where SuperView: UIView
     open func alignVertically(top: CGFloat,
                               spacing: CGFloat,
                               bottom: CGFloat,
-                              topLeft: CGFloat,
-                              bottomLeft: CGFloat,
-                              topRight: CGFloat,
-                              bottomRight: CGFloat) -> UILayoutDouble {
+                              topLeft: CGFloat?,
+                              bottomLeft: CGFloat?,
+                              topRight: CGFloat?,
+                              bottomRight: CGFloat?) -> UILayoutDouble {
         
         subView1.topAnchor.constraint(equalTo: superView.topAnchor, constant: top).isActive = true
-        subView1.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: topLeft).isActive = true
-        subView1.trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -topRight).isActive = true
-        
-        subView2.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: bottomLeft).isActive = true
-        subView2.trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -bottomRight).isActive = true
         subView2.bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -bottom).isActive = true
         
+        topLeft.map { subView1.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: $0).isActive = true }
+        topRight.map { subView1.trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -$0).isActive = true }
+        bottomLeft.map { subView2.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: $0).isActive = true }
+        bottomRight.map { subView2.trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -$0).isActive = true }
+        
         subView2.topAnchor.constraint(equalTo: subView1.bottomAnchor, constant: spacing).isActive = true
+        
+        subView1.centerXAnchor.constraint(equalTo: superView.centerXAnchor).isActive = true
+        subView2.centerXAnchor.constraint(equalTo: superView.centerXAnchor).isActive = true
         
         return self
     }
